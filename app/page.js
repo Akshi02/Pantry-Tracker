a37f814c67cc600f0fb7308ffb1253445e4546cb
Akshita Sushil Bhatia <asb272@uowmail.edu.au>
2024-08-04 16:57:50 +0400

    MODIFIED

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
-import {Box} from '@mui/material/Box';
+import * as React from 'react';
+import Box from '@mui/material/Box';
 
 export default function Home() {
   return (
-    <Box>
-      width: 100vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    </Box>
+  <Box
+    width="100vw"
+    height="100vh"
+    display={'flex'}
+    justifyContent={'center'}
+    alignItems={'center'}
+  >
+    Hello, world!
+  </Box>
   );
 }
