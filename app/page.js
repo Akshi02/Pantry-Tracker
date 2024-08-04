a71166778d839c473febb06b740cab192188353c
Akshita Sushil Bhatia <asb272@uowmail.edu.au>
2024-08-04 17:55:29 +0400

    MODIFIED

--- app/page.js
+++ app/page.js
@@ -1,5 +1,48 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import { Stack, Typography } from '@mui/material';
+
+const items = ['apple',
+  'banana',
+  'orange',
+  'grapefruit',
+  'grapes',
+  'strawberries',
+  'blueberries',
+  'raspberries',
+  'blackberries',
+  'peach',
+  'nectarine',
+  'plum',
+  'pear',
+  'kiwi',
+  'mango',
+  'pineapple',
+  'watermelon',
+  'cantaloupe',
+  'tomato',
+  'potato',
+  'onion',
+  'garlic',
+  'ginger',
+  'carrot',
+  'celery',
+  'cucumber',
+  'bell pepper',
+  'broccoli',
+  'cauliflower',
+  'asparagus',
+  'lettuce',
+  'spinach',
+  'kale',
+  'arugula',
+  'cabbage',
+  'zucchini',
+  'squash',
+  'pumpkin',
+  'sweet potato',
+  'corn']
+
 
 export default function Home() {
   return (
@@ -9,8 +52,44 @@ export default function Home() {
     display={'flex'}
     justifyContent={'center'}
     alignItems={'center'}
+    flexDirection={'column'}
   >
-    Hello, world!
+  <Box border={'1px solid #333'}>
+    <Box 
+    width={"800px"} 
+    height={"100px"} 
+      bgcolor={"#ADD8E6"} 
+      display={'flex'} 
+      justifyContent={'center'} 
+      alignItems={'center'} 
+      >
+        <Typography variant='h2' color={'#333'} textAlign={'center'}>
+          Pantry Items
+        </Typography>
+      </Box>
+      <Stack width={"800px"} height={"160px"} spacing={2} overflow={'auto'}>
+        {items.map((item) => (
+          <Box
+            key={item}
+            width={"100%"}
+            height={"300px"}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            bgcolor={"#f5f5f5"}
+            borderRadius={2}
+          >
+            <Typography 
+            variant={"h4"}
+            color={'#333'}
+            textAlign={'center'}
+            >
+            {item.charAt(0).toUpperCase() + item.slice(1)}
+            </Typography>
+          </Box>
+        ))}
+      </Stack>
+    </Box>
   </Box>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyASVm-rKRdQqUA43ECJ31yMYeAJ3gDnzNo",
+  authDomain: "pantrytracker-d4fbe.firebaseapp.com",
+  projectId: "pantrytracker-d4fbe",
+  storageBucket: "pantrytracker-d4fbe.appspot.com",
+  messagingSenderId: "21047209538",
+  appId: "1:21047209538:web:33ba094a2d85d87e1787db",
+  measurementId: "G-WXCT4NT4RE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
